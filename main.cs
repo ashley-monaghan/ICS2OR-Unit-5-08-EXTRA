c243941b77336feb42bfdc7b15d2178fbe801b0d
Ash! <113056162+ashley-monaghan@users.noreply.github.com>
2022-12-09 02:19:02 +0000

    whitespace fix 1

--- main.cs
+++ main.cs
@@ -13,17 +13,18 @@ class Program
         diviser = Convert.ToDouble(Console.ReadLine());
         Console.Write("Enter the second number: ");
         secondNumber = Convert.ToDouble(Console.ReadLine());
-        
+
         double counter = 0;
 
-              
-        while (diviser >= Math.Abs(secondNumber)) {
-        diviser = diviser - Math.Abs(secondNumber);
+        while (diviser >= Math.Abs(secondNumber)) 
+        {
+        diviser = diviser - Math.Abs(secondNumber
+        );
         counter++;
         };
-        
+
         Console.WriteLine("The answer is: " + counter + " R" + diviser + "!");
-      
+
         Console.WriteLine("");
         Console.Write("/Done");
     }
