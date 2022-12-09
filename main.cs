725ab7e6f7d87a4b1f681d1673739def5fc6d57b
Ash! <113056162+ashley-monaghan@users.noreply.github.com>
2022-12-09 02:30:12 +0000

    sync

--- main.cs
+++ main.cs
@@ -13,18 +13,13 @@ class Program
         diviser = Convert.ToDouble(Console.ReadLine());
         Console.Write("Enter the second number: ");
         secondNumber = Convert.ToDouble(Console.ReadLine());
-
         double counter = 0;
-
         while (diviser >= Math.Abs(secondNumber))
         {
-        diviser = diviser - Math.Abs(secondNumber
-        );
-        counter++;
+            diviser = diviser - Math.Abs(secondNumber);
+            counter++;
         };
-
         Console.WriteLine("The answer is: " + counter + " R" + diviser + "!");
-
         Console.WriteLine("");
         Console.Write("/Done");
     }
