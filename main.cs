653e3c4fd28859fe476f78ddf395a2c64aa28f46
Ash! <113056162+ashley-monaghan@users.noreply.github.com>
2022-12-09 02:04:39 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,30 @@
+using System;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        System.Random random = new System.Random();
+        // This function accepts user input
+        double diviser;
+        double secondNumber;
+        Console.Write("Enter two numbers to find the diviser!");
+        Console.WriteLine("");
+        Console.Write("Enter the first number: ");
+        diviser = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Enter the second number: ");
+        secondNumber = Convert.ToDouble(Console.ReadLine());
+        
+        double counter = 0;
+
+              
+        while (diviser >= Math.Abs(secondNumber)) {
+        diviser = diviser - Math.Abs(secondNumber);
+        counter++;
+        };
+        
+        Console.WriteLine("The answer is: " + counter + " R" + diviser + "!");
+      
+        Console.WriteLine("");
+        Console.Write("/Done");
+    }
+}
